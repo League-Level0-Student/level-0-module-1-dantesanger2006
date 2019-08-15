15770f5ece23df5ad97b8f24cd98a5925a0e53dd
dantesanger2006 <52478516+dantesanger2006@users.noreply.github.com>
2019-08-14 18:09:09 -0700

    Add files via upload

---
+++ bager.java
@@ -0,0 +1,51 @@
+package _12_badgers;
+
+import javax.swing.JOptionPane;
+
+public class bager {
+	public static void main(String[] args) {
+
+		/*
+		 * 
+		 * Badger, badger, badger Badger, badger, badger Badger, badger, badger Badger,
+		 * badger, badger Mushroom, mushroom
+		 * 
+		 * A badger, badger Badger, badger, badger Badger, badger, badger Badger,
+		 * badger, badger Mushroom, mushroom
+		 * 
+		 * A badger, badger Badger, badger, badger Badger, badger, badger Badger,
+		 * badger, badger Mush-mushroom
+		 * 
+		 * A badger, badger Badger, badger, badger Badger, badger, badger Badger,
+		 * badger, badger
+		 * 
+		 * A snake, a snake Snaaake! A snaaaake Oooh, it's a snake
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * ghj
+		 * 
+		 * dsmdtd
+		 * 
+		 * 
+		 */
+
+		for (int i = 0; i <4; i++) {
+
+			for (int no = 0; no < 12; no++) {
+
+				System.out.println("Badger");
+				if (no == 11 && i != 3) {
+					System.out.println("mushrom");
+
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("A snake, a snake Snaaake! A snaaaake Oooh, it's a snake");
+		
+	}
+
+}
